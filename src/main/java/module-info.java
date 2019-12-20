07e5fe09c04b53dc352d02fb9c0298ece0c315c4
Roby <roberto.portoni@gmail.com>
2019-12-20 21:47:18 +0100

    resolve modules dependecies

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module stock.ui {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.core;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+
+    //opens ->  needed for reflection calls from spring core
+    //and exports -> neeede because springboot use StockUiApplication
+    //with launch()
+    exports com.mechanitis.demo.stockui;
+    opens com.mechanitis.demo.stockui to spring.core;
+}
