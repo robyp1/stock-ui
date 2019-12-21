36271ed6bab45ad7bb70f4d14e7ee4ebf10faa47
Roby <roberto.portoni@gmail.com>
2019-12-21 18:14:54 +0100

    paint charts with call to stock-client  (to be finish again..)

--- src/main/java/com/mechanitis/demo/stockui/ChartController.java
+++ src/main/java/com/mechanitis/demo/stockui/ChartController.java
@@ -1,11 +1,46 @@
 package com.mechanitis.demo.stockui;
 
+import com.mechanitis.demo.stockclient.StockPrice;
+import com.mechanitis.demo.stockclient.WebClientStockClient;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
+/**
+ * This is component call from fxml, it set data in the line charts
+ */
 @Component
-public class ChartController {
+public class ChartController implements Consumer<StockPrice> {
     @FXML
-    public LineChart<String,Double> chart;
+    public LineChart<String, Double> chart;
+    private WebClientStockClient stockClientProxy;
+    private ObservableList<XYChart.Data<String, Double>> seriesData = FXCollections.observableArrayList();
+
+
+    public ChartController(WebClientStockClient stockClientProxy) {
+        this.stockClientProxy = stockClientProxy;
+    }
+
+    @FXML
+    public void initialize() {
+        ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
+        data.add(new XYChart.Series<>(seriesData));
+        chart.setData(data);
+        stockClientProxy.pricesFor("SYMBOL").subscribe(this);
+    }
+
+    @Override
+    public void accept(StockPrice stockPrice) {
+        //tell the ui thread to update chart every time new data accepted
+        Platform.runLater(() -> {
+            seriesData.add(new XYChart.Data<String, Double>(String.valueOf(stockPrice.getTime().getSecond()),
+                    stockPrice.getPrice()));
+        });
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,10 +8,12 @@ module stock.ui {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.sql;
+    requires stock.client;
 
     //opens ->  needed for reflection calls from spring core
     //and exports -> neeede because springboot use StockUiApplication
     //with launch()
     exports com.mechanitis.demo.stockui;
     opens com.mechanitis.demo.stockui to spring.core;
+
 }
