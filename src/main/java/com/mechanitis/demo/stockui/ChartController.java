60c842a77687f6766e4973372bfab8d7503306d2
Roby <roberto.portoni@gmail.com>
2019-12-22 16:09:38 +0100

    javfx border pane + rearrange code

--- src/main/java/com/mechanitis/demo/stockui/ChartController.java
+++ src/main/java/com/mechanitis/demo/stockui/ChartController.java
@@ -3,16 +3,25 @@ package com.mechanitis.demo.stockui;
 import com.mechanitis.demo.stockclient.StockPrice;
 import com.mechanitis.demo.stockclient.WebClientStockClient;
 import javafx.application.Platform;
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.scene.control.Label;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.function.Consumer;
+
+import static java.lang.String.*;
+import static javafx.collections.FXCollections.observableArrayList;
 
 /**
  * This is component call from fxml, it set data in the line charts
@@ -21,8 +30,10 @@ import java.util.function.Consumer;
 public class ChartController implements Consumer<StockPrice> {
     @FXML
     public LineChart<String, Double> chart;
+    @FXML
+    public Label lastPriceLbl;
     private WebClientStockClient stockClientProxy;
-    private ObservableList<XYChart.Data<String, Double>> seriesData = FXCollections.observableArrayList();
+    private ObservableList<XYChart.Data<String, Double>> seriesData = observableArrayList();
     private static final Logger log = LogManager.getLogger(ChartController.class);
 
 
@@ -32,10 +43,11 @@ public class ChartController implements Consumer<StockPrice> {
 
     @FXML
     public void initialize() {
-        ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
-        data.add(new XYChart.Series<>(seriesData));
+        String symbol = "EUR";
+        ObservableList<XYChart.Series<String, Double>> data = observableArrayList();
+        data.add(new Series<>(symbol,seriesData));
         chart.setData(data);
-        stockClientProxy.pricesFor("SYMBOL").subscribe(this);
+        stockClientProxy.pricesFor(symbol).subscribe(this);//handler is method accept in this class
         //log.info(take.blockFirst().getTime());
 
     }
@@ -44,8 +56,14 @@ public class ChartController implements Consumer<StockPrice> {
     public void accept(StockPrice stockPrice) {
         //tell the ui thread to update chart every time new data accepted
         Platform.runLater(() -> {
-            seriesData.add(new XYChart.Data<String, Double>(String.valueOf(stockPrice.getTime().getSecond()),
+            seriesData.add(new Data<String, Double>(valueOf(stockPrice.getTime().getSecond()),
                     stockPrice.getPrice()));
+            Locale locale = Locale.getDefault();
+            DecimalFormat df = (DecimalFormat) NumberFormat.getInstance(locale);
+            df.setParseBigDecimal(true);
+            df.setMaximumFractionDigits(10);
+            String price =  df.format(BigDecimal.valueOf(stockPrice.getPrice()));
+            lastPriceLbl.setText(price);
         });
     }
 }
